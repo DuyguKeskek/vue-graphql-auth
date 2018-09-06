462e2aef15aaa1fdcc868df19adcf07b0e496508
DuyguKeskek <duygukeskek@hotmail.com>
2018-09-06 16:40:30 +0300

    created form using bootstrap-vue

---
+++ src/components/AppLogin.vue
@@ -0,0 +1,97 @@
+<template>
+  <div>
+    <h4>{{login ? 'Login' : 'Sign Up'}}</h4>
+    <div>
+      <b-form-input
+        v-show="!login"
+        v-model="name"
+        type="text"
+        placeholder="Your name">
+      </b-form-input>
+      <b-form-input
+        v-model="email"
+        type="text"
+        placeholder="Your email address">
+      </b-form-input>
+      <b-form-input
+        v-model="password"
+        type="password"
+        placeholder="Password">
+      </b-form-input>
+    </div>
+    <b-button type="submit" variant="primary"
+        @click="confirm()">
+        {{login ? 'Login' : 'Create Account'}}
+    </b-button>
+    <b-button type="submit" variant="danger"
+        @click="login = !login">
+        {{login ? 'Sign Up' : 'Already have an account?'}}
+    </b-button>
+    </div>
+</template>
+
+<script>
+import { GC_USER_ID, GC_AUTH_TOKEN } from '../constants/settings'
+import { CREATE_USER_MUTATION, SIGNIN_USER_MUTATION } from '../constants/graphql'
+
+export default {
+  name: 'AppLogin',
+  data () {
+    return {
+      email: '',
+      login: true,
+      name: '',
+      password: ''
+    }
+  },
+  methods: {
+    confirm () {
+      const { name, email, password } = this.$data
+      if (this.login) {
+        this.$apollo.mutate({
+          mutation: SIGNIN_USER_MUTATION,
+          variables: {
+            email,
+            password
+          }
+        }).then((result) => {
+          const id = result.data.signinUser.user.id
+          const token = result.data.signinUser.token
+          this.saveUserData(id, token)
+          console.log(result)
+        }).catch((error) => {
+          alert(error)
+        })
+      } else {
+        this.$apollo.mutate({
+          mutation: CREATE_USER_MUTATION,
+          variables: {
+            name,
+            email,
+            password
+          }
+        }).then((result) => {
+          const id = result.data.signinUser.user.id
+          const token = result.data.signinUser.token
+          this.saveUserData(id, token)
+        }).catch((error) => {
+          alert(error)
+        })
+      }
+      this.$router.push({path: '/'})
+    },
+    saveUserData (id, token) {
+      localStorage.setItem(GC_USER_ID, id)
+      localStorage.setItem(GC_AUTH_TOKEN, token)
+      this.$root.$data.userId = localStorage.getItem(GC_USER_ID)
+    }
+  }
+}
+</script>
+<style>
+.form-control {
+    display: block;
+    margin: 0 auto;
+    width: auto;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import AppLogin from '@/components/AppLogin'
 
 Vue.use(Router)
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: AppLogin
     }
-  ]
+  ],
+  mode: 'history'
 })
