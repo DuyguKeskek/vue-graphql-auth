c13c101a1538db8e0016860aa0d9dbf9cd2f76e9
DuyguKeskek <duygukeskek@hotmail.com>
2018-09-06 15:13:09 +0300

    imported apollo-client and vue-apollo packages

--- src/main.js
+++ src/main.js
@@ -1,15 +1,45 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 import Vue from 'vue'
+
+import VueApollo from 'vue-apollo'
+
 import App from './App'
 import router from './router'
 
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+
 Vue.config.productionTip = false
+
+const httpLink = new HttpLink({
+  // You should use an absolute URL here
+  uri: '__SIMPLE_API_ENDPOINT__'
+})
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  connectToDevTools: true
+})
+
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+  defaultOptions: {
+    $loadingKey: 'loading'
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  provide: apolloProvider,
   router,
-  components: { App },
-  template: '<App/>'
+  render: h => h(App)
 })
